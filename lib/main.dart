adfd7d4c028e1cad19c0649db556d877c5bc8bf1
JongMin <standbyme227@algorithmlabs.co.kr>
2019-12-09 12:47:05 +0900

    First Flutter c2
    무한스크롤 완성 후 재작성

--- lib/main.dart
+++ lib/main.dart
@@ -8,26 +8,57 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 //    final wordPair = new WordPair.random(); // 이곳에서 만드는게 아님
     return MaterialApp(
-      title: 'Welcome To New Package',
-      home: Scaffold(
-        appBar: new AppBar(
-          title: Text('Called English Words'),
-        ),
-        body: Center(
-//          child: new Text(wordPair.asPascalCase), // 위에서 만들었기때문에 사용했었음
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+//      title: 'Welcome To New Package',
+//      home: Scaffold(
+//        appBar: new AppBar(
+//          title: Text('Called English Words'),
+//        ),
+//        body: Center(
+////          child: new Text(wordPair.asPascalCase), // 위에서 만들었기때문에 사용했었음
+//          child: RandomWords(),
+//        ),
+//      ),
     );
   }
 }
 
 class RandomWordsState extends State<RandomWords> {
-  
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Startup Name Generator'),
+        ),
+        body: _buildSuggestions(),
+    );
+//    final wordPair = WordPair.random();
+//    return Text(wordPair.asPascalCase);
+  }
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        }
+    );
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
 
